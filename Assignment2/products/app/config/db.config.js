0fefad2c4acba3347af19bcf83d3144a122e8b0a
maggielaitszshuen <tlai34@my.centennialcollege.ca>
2023-06-16 17:09:37 -0400

    Push Assignment 2

---
+++ Assignment2/products/app/config/db.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    //url: "mongodb://localhost:27017/Blessing_db"
+  
+    //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+  
+     //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+  
+  url:"mongodb+srv://maggielaitszshuen:9V7Dnuaw@cluster0.khp8vne.mongodb.net/DressStore?retryWrites=true&w=majority"
+  
+  };
